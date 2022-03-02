420c1c73b9a98f9112e8068097abb140c2ff4abc
Taylor Bird <nsubordin81@gmail.com>
2022-03-02 18:03:11 -0500

    [feat] adding insertion sort
    don't have a strict module system set up yet, so jamming
    all the algorithms into the Main.scala file for now. hoping to
    expand that to include different versions of eaqch of the algorithms
    as I learn them.

--- introduction-to-algorithms/src/main/scala/Main.scala
+++ introduction-to-algorithms/src/main/scala/Main.scala
@@ -1,5 +1,24 @@
 @main def hello: Unit = 
   println("Hello world!")
-  println(msg)
+  println(insertionSort(2::4::3::1::Nil))
 
-def msg = "I was compiled by Scala 3. :)"
+
+// simpler instance of unordered list, don't like so much
+// it is a faithful more or less reproduction of the psuedocode 
+// but it breaks some scala rules abou mutable state
+// I'd rather have this accomplished with recursion than assignment
+def place(unordered: List[Int], j: Int, key: Int): List[Int] = 
+  var i = j - 1
+  var unsorted: List[Int] = unordered
+  while (i >= 0 && unsorted(i) > key) do
+    unsorted = unsorted.updated(i+1, unsorted(i))
+    i = i - 1
+  unsorted.updated(i + 1, key)
+
+
+def insertionSort(unsorted: List[Int]) =
+  var returnList: List[Int] = unsorted
+  for (x, j) <- unsorted.zipWithIndex
+    if j >= 1
+  do returnList = place(returnList, j, x)
+  returnList
