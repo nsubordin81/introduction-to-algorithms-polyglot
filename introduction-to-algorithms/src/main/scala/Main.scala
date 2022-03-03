3051a1e7833abc19962e7b61879be7e00132af6a
Taylor Bird <nsubordin81@gmail.com>
2022-03-02 21:45:35 -0500

    [docs] add comments to insertion sort 1
    comments regarding the loop invariant, need to be
    able to use loop invariants as one of the key tools to
    prove the correctness of algorithms.

--- introduction-to-algorithms/src/main/scala/Main.scala
+++ introduction-to-algorithms/src/main/scala/Main.scala
@@ -18,6 +18,13 @@ def place(unordered: List[Int], j: Int, key: Int): List[Int] =
 
 def insertionSort(unsorted: List[Int]) =
   var returnList: List[Int] = unsorted
+  // loop invariant, when we start this array, the values in the subarray
+  // from 0 to j - 1 are the elements that were in those positions in the 
+  // original array passed in but they will be in sorted order
+  // initialization: true because j-1 will equal 0 so it is inherently sorted
+  // maintenance: it stays true for each iteration following that because place sorts that subarray
+  // termination: when you get to the termination of the loop, returnList will have placed the final element x
+  // into the subarray and returned that as the result. Therefore we have sorted the full array
   for (x, j) <- unsorted.zipWithIndex
     if j >= 1
   do returnList = place(returnList, j, x)
