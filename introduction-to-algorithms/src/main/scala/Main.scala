d50fafe29989b4416d30231fe62efd3e0a8c67c3
Taylor Bird <nsubordin81@gmail.com>
2022-03-03 18:21:39 -0500

    [feat] insertion sort backwards with HOFunction
    create a function parameter that allows me to specify
    the direction that the insertion sort will take

--- introduction-to-algorithms/src/main/scala/Main.scala
+++ introduction-to-algorithms/src/main/scala/Main.scala
@@ -7,14 +7,25 @@
 // it is a faithful more or less reproduction of the psuedocode 
 // but it breaks some scala rules abou mutable state
 // I'd rather have this accomplished with recursion than assignment
-def place(unordered: List[Int], j: Int, key: Int): List[Int] = 
+def place(unordered: List[Int], j: Int, key: Int, comparison: (List[Int], Int, Int) => Boolean): List[Int] = 
   var i = j - 1
   var unsorted: List[Int] = unordered
-  while (i >= 0 && unsorted(i) > key) do
+  while (i >= 0 && comparison(unsorted, i, key) ) do
     unsorted = unsorted.updated(i+1, unsorted(i))
     i = i - 1
   unsorted.updated(i + 1, key)
 
+def ascendingRule(l: List[Int], i: Int, key: Int): Boolean =
+  l(i) > key
+
+
+// corresponds to exercise 2.1-2, this works becuase the only thing we have
+// to change is the maintenance portion of the loop invariant. the list still
+// starts with the first element as the only thing in the ordered array, 
+// so what we said here is we are going to go thorugh and shuffle things to 
+// the right only if they are smaller, so the array will grow but be  sorted in reverse
+def descendingRule(l: List[Int], i: Int, key: Int): Boolean =
+  l(i) < key
 
 def insertionSort(unsorted: List[Int]) =
   var returnList: List[Int] = unsorted
@@ -27,5 +38,5 @@ def insertionSort(unsorted: List[Int]) =
   // into the subarray and returned that as the result. Therefore we have sorted the full array
   for (x, j) <- unsorted.zipWithIndex
     if j >= 1
-  do returnList = place(returnList, j, x)
+  do returnList = place(returnList, j, x, descendingRule)
   returnList
